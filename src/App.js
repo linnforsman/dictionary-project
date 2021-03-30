d5ae4058488d0c4f622cada8af8d7701cca66ece
Linn Forsman <linnmetal7@gmail.com>
2021-03-30 11:48:52 +0200

    Added search engine + dictionary api integration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from "./shecodes.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-        <img src={logo} className="App-logo img-fluid" alt="logo"/>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer text-center">
+          <small>This application is built with Bootstrap and React framework by Linn Forsman.</small>
+        </footer>
+      </div>
     </div>
   );
 }
