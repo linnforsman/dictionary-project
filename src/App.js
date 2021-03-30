5d18d8f3228cb6fb6c1d9c3b00edd5a39288103d
Linn Forsman <linnmetal7@gmail.com>
2021-03-30 10:21:32 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./shecodes.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello
+        <img src={logo} className="App-logo img-fluid" alt="logo"/>
+      </header>
+    </div>
+  );
+}
